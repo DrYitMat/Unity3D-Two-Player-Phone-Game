4fa73497291072845a4c1d3ff0e22df5cc9e7e41
Wyatt Turner <wyattturner97@gmail.com>
2015-09-17 04:10:51 -0600

    I was tired and screwed up merge with master. This is basically a re-commit

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+/// <summary>
+/// Counts down to the start of the game. 
+/// 
+/// ToggleObjects can be used by other scripts to toggle the UI, if needed. 
+/// </summary>
+public class Countdown : MonoBehaviour {
+
+	// Countdown text, IE 3..2..1..GO
+	private Text countText;
+
+	// List of objects to toggle on/off, when needed. 
+	private List<Transform> objectsToToggle = new List<Transform>();
+
+	// Init
+	void Start() {
+		countText = gameObject.GetComponent<Text>();
+		objectsToToggle.Add(GameObject.FindGameObjectWithTag("PlayerOneUI").transform);
+		objectsToToggle.Add(GameObject.FindGameObjectWithTag("PlayerTwoUI").transform);
+		StartCoroutine(Count());
+	}
+
+	// Start the countdown timer over. 
+	public void Reset() {
+		StartCoroutine(Count());
+	}
+
+	/// <summary>
+	/// Toggles all objects in the objectsToToggle list on or off.
+	/// 
+	/// if t == true objects will be set active (visibile in game). if t == false objects will be set inactive (not visible in game).
+	/// </summary>
+	/// <param name="t">If set to <c>true</c> t.</param>
+	public void ToggleObjects(bool t) {
+		foreach(Transform a in objectsToToggle) {
+			foreach(Transform b in a) {
+				b.gameObject.SetActive(t);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Countdown to the start of a new game. This is a three second countdown. 
+	/// 
+	/// Toggle the objects off before the start of the countdown, toggle the objects on at the end of the countdown. 
+	/// </summary>
+	private IEnumerator Count() {
+		ToggleObjects(false);
+		countText.text = "3";
+		yield return new WaitForSeconds(1.0f);
+		countText.text = "2";
+		yield return new WaitForSeconds(1.0f);
+		countText.text = "1";
+		yield return new WaitForSeconds(1.0f);
+		countText.text = "GO!";
+		ToggleObjects(true);
+		yield return new WaitForSeconds(1.0f);
+		countText.text = "";
+	}
+}
---
+++ Assets/Scripts/SetUI.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script allows the UI to be easily adapted to different situations, with attack and defend functions added to the UI buttons.
+/// 
+/// Hierarchy should this: 
+/// 
+/// Parent (this script)
+/// 	AttackParent
+/// 		Top
+/// 		Sides
+/// 		Bottom
+/// 	DefendParent
+/// 		Top
+/// 		Sides
+/// 		Bottom
+/// </summary>
+/// 
+/// TODO: Add more flexibility by counting the children and adding listerns accordingly. 
+public class SetUI : MonoBehaviour {
+
+	private Attack playerAttack;
+	private Defend playerDefend;
+	private Transform attack,defend;
+
+	void Start() {
+		setPlayerObject();
+		attack = transform.GetChild(0);
+		defend = transform.GetChild(1);
+		createListeners();
+	}
+
+	private void setPlayerObject() {
+		if(gameObject.CompareTag("PlayerOneUI")) {
+			playerAttack = GameObject.FindGameObjectWithTag("PlayerOne").GetComponent<Attack>();
+			playerDefend = GameObject.FindGameObjectWithTag("PlayerOne").GetComponent<Defend>();
+		} else if(gameObject.CompareTag("PlayerTwoUI")) {
+			playerAttack = GameObject.FindGameObjectWithTag("PlayerTwo").GetComponent<Attack>();
+			playerDefend = GameObject.FindGameObjectWithTag("PlayerTwo").GetComponent<Defend>();
+		}
+		else Debug.Log("Could not set player.");
+
+		gameObject.transform.rotation = playerAttack.transform.rotation;
+	}
+
+	/// <summary>
+	/// Creates the listeners. Object order need to be as follows Top - Sides - Bottom
+	/// </summary>
+	private void createListeners() {
+		attack.GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(() => { playerAttack.Top();});
+		attack.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => { playerAttack.Sides();});
+		attack.GetChild(2).gameObject.GetComponent<Button>().onClick.AddListener(() => { playerAttack.Bottom();});
+		defend.GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(() => { playerDefend.Top();});
+		defend.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => { playerDefend.Sides();});
+		defend.GetChild(2).gameObject.GetComponent<Button>().onClick.AddListener(() => { playerDefend.Bottom();});
+	}
+
+
+
+}
