2383f5efd9dbcfed4b853e065476eefa678385ee
Wyatt Turner <wyattturner97@gmail.com>
2015-09-19 04:40:40 -0600

    Base commit for @mzdarsky

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ability : MonoBehaviour {
+
+	public AbilityType abilityType;
+	public string description;
+	public int damage;
+	public int cost;
+
+	//MY LOGIC 
+}
+
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ship : MonoBehaviour {
+
+	private ShipTypes shipType;
+
+	private static int MAX_ABILITIES = 3;
+
+	public List<Ability> abilityList = new List<Ability>();
+	public string description;
+
+	// Other things a ship should have.
+
+
+	//MY LOGIC
+
+	void Update () {
+		checkAbilityCount();
+	}
+
+	private void checkAbilityCount() {
+		if(abilityList.Count > MAX_ABILITIES){
+			Debug.Log("Cannot add ability");
+			abilityList.RemoveAt(abilityList.Count);
+		}
+	}
+}
