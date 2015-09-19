e906c5df5d49e58b48ff02361daec27a792ffdb3
Wyatt Turner <wyattturner97@gmail.com>
2015-09-19 04:44:19 -0600

    Reset. Better Attack, Defend. Created a static class to hold variables, called Globals. This has variables like, Cooldown times and wait times.

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -8,11 +8,3 @@ public static class Globals {
 	public static float BOTTOM_COOLDOWN = 3.0f;
 	public static float WAIT_TIME = 1.5F;
 }
-
-public enum ShipTypes {
-	fireShip, electripShip, shadowShip
-}
-
-public enum AbilityType {
-	fire,electric,shadow
-}
